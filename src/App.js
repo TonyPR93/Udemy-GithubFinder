5d76ca33ae788ab4837b73e4c601da4ff7a9031e
Panont Anthony <anthonypanont@gmail.com>
2023-09-07 23:47:47 +0200

    API Github / User, Repos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/search";
+import User from "./components/users/User";
 import { Alert } from "./components/layout/Alert";
 import { About } from "./components/pages/About";
 import axios from "axios";
@@ -13,8 +14,10 @@ import axios from "axios";
 class App extends Component {
   state = {
     users: [], //Contiendra tout les utilisateurs
+    user: {},
     loading: false, //Pour faire une animation avec le spinner.
     alert: null,
+    repos: [],
   };
 
   // async componentDidMount() {
@@ -36,18 +39,47 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  //get single github users
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+    );
+
+    this.setState({ user: res.data, loading: false });
+    console.log(this.props.user);
+  };
+
+  //get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    console.log(username);
+    try {
+      const res = await axios.get(
+        `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`,
+      );
+
+      console.log("Response from GitHub API:", res.data); // Ajoutez cette ligne pour déboguer la réponse de l'API
+
+      this.setState({ repos: res.data, loading: false });
+    } catch (error) {
+      console.error("Error fetching data from GitHub API:", error);
+      this.setState({ loading: false });
+    }
+  };
+
   //clear
   clearUsers = () => this.setState({ users: [], loading: false });
 
   //alert
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
-    console.log(this.state.alert);
     setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
     return (
       <Router>
         <div className="App">
@@ -71,6 +103,18 @@ class App extends Component {
               />
 
               <Route path="/about" Component={About}></Route>
+              <Route
+                path={`/user/:login`}
+                element={
+                  <User
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    loading={loading}
+                    repos={repos}
+                  />
+                }
+              />
             </Routes>
 
             {/* On envoie au component users le tableau contenant TOUT les utilisateurs */}
