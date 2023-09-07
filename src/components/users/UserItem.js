5d00eb31cf393346a688103d8b0a2ea20129df34
Panont Anthony <anthonypanont@gmail.com>
2023-09-07 23:58:06 +0200

    <a> to <Link>

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 // On recupere les données envoyés via le component Users
-const UserItem = ({user: {login, avatar_url, html_url}}) => {
-
-
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
-    <div className='card text-center'>
-      <img src={avatar_url} alt="" className='round-img' style={{width: '60px'}}></img>
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      ></img>
       <h3>{login}</h3>
 
       <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1'>
-              more
-          </a>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
+          more
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default UserItem
+export default UserItem;
